ec801cb3e7cb8f875812442f262aaba632e0857a
unknown <chenukasumanasekara@gmail.com>
2020-10-09 21:40:42 +0530

    Completed user profiles

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -8,5 +8,7 @@ const jwtHelper = require("../config/jwtHelper");
 router.post("/register", ctrlUser.register);
 router.post("/authenticate", ctrlUser.authenticate);
 router.get("/userProfile", jwtHelper.verifyJwtToken, ctrlUser.userProfile);
+router.get("/users", ctrlUser.getAllUsers);
+router.delete("/users/:id", ctrlUser.deleteUserById);
 
 module.exports = router;
