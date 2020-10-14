88ab369cf077b208b3d4faceeec5972a543b45a2
Chamodi Thennakoon <58078417+chamodit@users.noreply.github.com>
2020-10-14 19:35:01 +0530

    Createde some functions

---
+++ routes/requisition.routes.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const requisitionController = require("../controllers/requisition.controller");
+
+router.post('/requisitions', requisitionController.addRequisition);
+router.get('/requisitions', requisitionController.viewRequisition);
+router.get('/requisitions/:id', requisitionController.viewRequisitionById);
+router.put('/requisitions/:id', requisitionController.updaterequisitionById);
+router.put('/requisitions/:id', requisitionController.updateStatusById);
+router.get('/getRequisitionNumber', requisitionController.getNextRequisitionID);
+
+module.exports = router;
