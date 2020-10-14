82816de72f7ef55d4c7df6795fac886caaf88e87
unknown <chenukasumanasekara@gmail.com>
2020-10-14 17:27:57 +0530

    Fixed issues and completed suppliers

--- models/supplier.model.js
+++ models/supplier.model.js
@@ -1,16 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const supplierSchema = new mongoose.Schema(
-    {
-        supId : { type: String, required: true, unique: true },
-        supName : { type: String, required: true },
-        supLocation : { type: String, required: true },
-        supEmail : { type: String, required: true },
-        supTel : { type: Number, required: true },
-    },
+  {
+    supId: { type: String, required: true, unique: true },
+    supName: { type: String, required: true },
+    supLocation: { type: String, required: true },
+    supEmail: { type: String, required: true },
+    supTel: { type: String, required: true },
+  },
 
-    { timestamps: true }
-
+  { timestamps: true }
 );
 
-module.exports = mongoose.model( 'supplier', supplierSchema );
+module.exports = mongoose.model("supplier", supplierSchema);
