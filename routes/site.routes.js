72ceb4c5277d211b511406a27f8bcd2c44d85fc5
unknown <chenukasumanasekara@gmail.com>
2020-10-16 00:59:14 +0530

    Added auto-generation for site ID

--- routes/site.routes.js
+++ routes/site.routes.js
@@ -1,10 +1,11 @@
-const router = require('express').Router();
-const siteController = require('../controllers/site.controller');
+const router = require("express").Router();
+const siteController = require("../controllers/site.controller");
 
-router.post('/sites', siteController.addSite);
-router.get('/sites', siteController.viewSites);
-router.get('/sites/:id', siteController.viewSiteById);
-router.put('/sites/:id', siteController.updateSiteDetails);
-router.delete('/sites/:id', siteController.deleteSiteById);
+router.post("/sites", siteController.addSite);
+router.get("/sites", siteController.viewSites);
+router.get("/sites/:id", siteController.viewSiteById);
+router.put("/sites/:id", siteController.updateSiteDetails);
+router.delete("/sites/:id", siteController.deleteSiteById);
+router.get("/getSiteNumber", siteController.getNextSiteID);
 
 module.exports = router;
