ba3aafbe9b8102b968ba54ba82b87662812b52c0
unknown <chenukasumanasekara@gmail.com>
2020-10-17 17:41:14 +0530

    Added budget update

--- routes/site.routes.js
+++ routes/site.routes.js
@@ -7,5 +7,6 @@ router.get("/sites/:id", siteController.viewSiteById);
 router.put("/sites/:id", siteController.updateSiteDetails);
 router.delete("/sites/:id", siteController.deleteSiteById);
 router.get("/getSiteNumber", siteController.getNextSiteID);
+router.put("/sites/:id/budget", siteController.uppdateBudgetWhenApproved);
 
 module.exports = router;
